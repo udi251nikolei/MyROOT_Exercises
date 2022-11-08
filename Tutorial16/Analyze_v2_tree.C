0777d4d868859009c1b387c75342ec861de22025
Nikolei <113668786+udi251nikolei@users.noreply.github.com>
2022-11-08 19:17:10 +0100

    Update Analyze_v2_tree.C

--- Tutorial16/Analyze_v2_tree.C
+++ Tutorial16/Analyze_v2_tree.C
@@ -23,25 +23,9 @@ using namespace std;
 
 void analyze_v2_2particle() {
 
-// Creating a canvas and generating the histogram we want to plot data from file.
-	TCanvas* c1 = new TCanvas("c1", "v2 canvas", 900, 600);
-	TH1D* hPhi = new TH1D("hPhi", "ROOT file read phi distribution; phi; Counts", 100, 0, 2*TMath::Pi());
-
 // Sum of all seperate calculated <2> counter per even (Sec. 2.2)
 	Double_t avg_v2_std_allEvents = 0;
 	Double_t avg_v2_Qvector_allEvents = 0;
-	
-	//while(file >> helpString >> eventNo) {
-		//cout << "Reading event: " << eventNo << endl;
-		//nEvents++;
-
-		//Int_t nTracks = -1;
-		//file >> helpString >> nTracks;
-		//cout << "Event contains " << nTracks << " tracks" << endl;
-
-		//Int_t trackNo;
-		//string colon_string;
-		//Double_t phi[nTracks]; 
 
 // Open the file and get the tree
   	TFile* file = TFile::Open("phi_dist.root");
@@ -53,37 +37,70 @@ void analyze_v2_2particle() {
 
   	Int_t nEvents = tree->GetEntries();
 
-	cout << nEvents << endl; // Total size of nEvents -> 1000 events
+	cout << "Analyzing " << nEvents << " events..." << endl;
 
 	for(Int_t n = 0; n < nEvents; n++) {
     	// Read in event n (fills event and trackArray)
     	tree->GetEntry(n);
-		cout << "Entry no. " << n << endl; // Prints all Events
+		//cout << "Entry no. " << n << endl; // Prints all Events
+
 		const Int_t nTracks = trackArray->GetEntries();
-		cout << "This event has " << nTracks << " tracks" << endl; // For each event, number of Tracks inside that Event is printed
+		//cout << "This event has " << nTracks << " tracks" << endl; // For each event, number of Tracks inside that Event is printed
 
 		Double_t phi[nTracks];
 
 		for(Int_t nt = 0; nt < nTracks; nt++) {
 			MyTrack* track = (MyTrack*)trackArray->At(nt);
 			phi[nt] = track->fPhi; // Get all phi values within that event
-			cout << "Phi value for track element " << nt << " is " << phi[nt] << endl;
+			//cout << "Phi value for track element " << nt << " is " << phi[nt] << endl;
 
-			hPhi->Fill(phi[nt]);
+			//hPhi->Fill(phi[nt]);
 		}
+
+	// Starting calculations for on event at a time
+		// 2-particle standard method (Sec. 2.2)
+		Double_t sum_cos2_diff = 0;
+
+		// 2-particle Q vectors (Sec. 2.3)
+		Double_t sum_cos2 = 0;
+		Double_t sum_sin2 = 0;
+
+		for(Int_t s = 0; s < nTracks; s++) { 
+
+			// Computing Q vector
+			sum_cos2 += cos(2*phi[s]);
+			sum_sin2 += sin(2*phi[s]);
+
+			for(Int_t t = s+1; t < nTracks; t++) {
+				sum_cos2_diff += 2*cos(2*(phi[s]-phi[t]));
+			}
+		}
+
+		Double_t Double_nTracks = nTracks;
+
+		// Computing std <2> per Event
+		Double_t avg_v2_perEvent = 1/(Double_nTracks*(Double_nTracks - 1)) * sum_cos2_diff;
+		avg_v2_std_allEvents += avg_v2_perEvent; // Adding to total sum over all Events
+
+		// cout << "The calculated <2> for Event " << nEvents << " is " << avg_2_perEvent << endl;
+		// cout << "The total sum <2> after event " << nEvents <<  " is " << savg2_allEvents << endl;
+
+		// Computing Qvector <2> per Event
+		TComplex Q = TComplex(sum_cos2, sum_sin2);
+		Double_t Q2 = Q*Q; // Ignoring imaginary numbers
+		
+		Double_t avg_v2_perEvent_Qvector = (Q2 - Double_nTracks)/(Double_nTracks*(Double_nTracks - 1));
+		avg_v2_Qvector_allEvents += avg_v2_perEvent_Qvector;
 
 	}
 
-// Set ROOT drawing styles
-	gStyle->SetOptStat(1111);
-	gStyle->SetStatX(0.9);
-	gStyle->SetStatY(0.5);
-	gStyle->SetOptFit(1111);
+	// The calculated v2 from 2-particle method (Sec. 2.2)
+	Double_t v2_std = sqrt(avg_v2_std_allEvents /nEvents);
+	cout << "Std: v2 (average over " << nEvents << " events) = " << v2_std << endl;
 
-// Plotting the v2 function
-	hPhi->SetMinimum(0);
-	hPhi->Draw();
-  
-// Saving the canvas as a jpg file
-  	//c1->SaveAs("test.jpg");
+	// The calculated v2 from 2-particle method using the Q-vector (Sec. 2.3)
+	Double_t v2_Qvector = sqrt(avg_v2_Qvector_allEvents/nEvents);
+	cout << "Q-vectors: v2 (average over " << nEvents << " events) = " << v2_std << endl;
+
 }
+
